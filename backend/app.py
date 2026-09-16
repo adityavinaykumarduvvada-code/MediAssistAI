a0d976f6486b87ab0e2f5469a87953187869ec56
DUVVADA ADITYA VINAYKUMAR <adityavinaykumarduvvada@gmail.com>
2026-09-16 20:32:51 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -144,4 +144,4 @@ def get_session_report(session_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8001, reload=True)
